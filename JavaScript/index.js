a14093b6b16e43991f4654e4e2cc024284c19ded
Luis Alejandro Machado Orozco <106163225+Luisalejandro28@users.noreply.github.com>
2023-08-25 13:21:26 -0300

    V 0.0.2

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -5,7 +5,7 @@ let contacto = document.getElementById("Contacto");
 
 name.innerHTML = "Hola! Soy Luis Alejandro Machado";
 titulo.innerHTML = "Programador Web en Formación";
-about.innerHTML = `Desarrollador web en formacion, con un enfoque en la creación de sitios web interactivos y responsivos. Si bien no tengo experiencia laboral previa, he trabajado en proyectos personales y he realizado cursos en línea para seguir mejorando mis habilidades. Soy una persona responsable y proactiva, con un fuerte enfoque en el trabajo en equipo y la solución de problemas. También disfruto aprendiendo cosas nuevas y siempre estoy buscando maneras de mejorar.`
+about.innerHTML = `Desarrollador web en formacion, con un enfoque en la creación de sitios web interactivos y responsivos. He trabajado en proyectos personales y he realizado cursos en línea para seguir mejorando mis habilidades. Soy una persona responsable y proactiva, con un fuerte enfoque en el trabajo en equipo y la solución de problemas. También disfruto aprendiendo cosas nuevas y siempre estoy buscando maneras de mejorar.`
 contacto.innerHTML = "Contacto"
 
 const menuItems = document.querySelectorAll('.nav-item');
